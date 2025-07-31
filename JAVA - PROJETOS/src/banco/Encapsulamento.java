239e57a095480e860bf05d30a1ae28471ebb23b3
Daniel Maciel Benício <124325178+danielmacielbenicio@users.noreply.github.com>
2025-07-31 11:27:00 -0300

    Adicionar arquivos (BANCO/ENCAPSULAMENTO)

---
+++ JAVA - PROJETOS/src/banco/Encapsulamento.java
@@ -0,0 +1,17 @@
+package banco;
+
+public class Pessoa {
+	private String nome;
+	private String idade;
+	private String saldo;
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getIdade() {
+		return idade;
+	}
+	public String getSaldo() {
+		return saldo;
+	}
+}
