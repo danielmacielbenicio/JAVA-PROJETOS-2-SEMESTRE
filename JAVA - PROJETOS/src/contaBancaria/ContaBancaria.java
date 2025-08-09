1d92ba8b84b06c0c8df5fc8338d22ee68070ebe6
Daniel Maciel Benício <124325178+danielmacielbenicio@users.noreply.github.com>
2025-08-09 18:42:24 -0300

    Atualizar arquivos conta Bancaria

--- JAVA - PROJETOS/src/contaBancaria/ContaBancaria.java
+++ JAVA - PROJETOS/src/contaBancaria/ContaBancaria.java
@@ -1,29 +1,55 @@
-package banco;
+	package banco;
 
-import java.util.Scanner;
+	import java.util.Scanner;
 
-public class ContaBancaria {
+	public class ContaBancaria {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Seja bem vindo ao banco PedMais");
-		System.out.println("ESCOLHA A OPERAÇÃO [1] SAQUE [2] DEPOSITO [3] CONSULTA");
-		
-		int opcoes = sc.nextInt();
-		String escolha;
-		
-		switch (opcoes) {
-		
-		case 1:
-		System.out.println("Você escolheu a opção de saque - Digite o quanto quer sacar:");
-		sc.next();
-		case 2:
-		sc.next();
-		case 3:
-		sc.nextDouble();
-		
-		}
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        boolean sair = false;
+	        Pessoa pessoa = new Pessoa(1000.0); // saldo inicial de 1000 reais.
+	        
+	        while (!sair) {
+	        	System.out.println();
+	            System.out.println("Seja bem vindo ao banco PedMais");
+	            System.out.println();
+	            System.out.println("ESCOLHA A OPERAÇÃO: SAQUE [1] DEPOSITO [2] SAIR [3]");
+	            System.out.println("Você tem " + pessoa.getSaldo() + " R$ em sua carteira virtual.");
+	            int opcoes = sc.nextInt();
+	            double valor;
+
+	            switch (opcoes) {
+	                case 1:
+	                    System.out.println("Você escolheu a opção de saque - Digite o quanto quer sacar:");
+	                    valor = sc.nextDouble();
+	                    System.out.println();
+	                    if (valor > 0 && valor <= pessoa.getSaldo()) {
+	                        pessoa.sacar(valor);
+	                        System.out.println("Saque realizado! Novo saldo: " + pessoa.getSaldo());
+	                    } else {
+	                        System.out.println("Valor inválido. O valor deve ser maior que 0 e menor que seu saldo");
+	                        System.out.println();
+	                    }
+	                    break;
+	                case 2:
+	                    System.out.println("Você escolheu a opção de depósito - Digite o quanto quer depositar:");
+	                    valor = sc.nextDouble();
+	                    if (valor > 0) {
+	                        pessoa.depositar(valor);
+	                        System.out.println("Depósito realizado! Novo saldo: " + pessoa.getSaldo());
+	                        System.out.println();
+	                    } else {
+	                        System.out.println("Valor inválido. O valor deve ser maior que 0.");
+	                    }
+	                    break;
+	                case 3:
+	                    sair = true;
+	                    System.out.println("Você saiu com sucesso!");
+	                    break;
+	                default:
+	                    System.out.println("Opção inválida.");
+	            }
+	        }
+	        sc.close();
+	    }
 	}
-	
-
-}
--- JAVA - PROJETOS/src/contaBancaria/Pessoa.java
+++ JAVA - PROJETOS/src/contaBancaria/Pessoa.java
@@ -1,22 +1,25 @@
 package banco;
 
-public class Pessoa{
-	private double saq;
-	private double dep;
-	private double sal;
-	
-	public void setSaque()  {
-			
-	}
-	
-	public double getSaque() {
-		return saq;
-	}
-	public double getDeposito() {
-		return dep;
-	}
-	public double getSaldo() {
-		return sal;
-	}
-	
+public class Pessoa {
+    private double saldo;
+
+    public Pessoa(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo = saldo - valor;
+        }
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo = saldo + valor;
+        }
+    }
 }
